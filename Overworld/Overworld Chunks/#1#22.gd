2766243f8d7b35d6b8db1c4d91ac2579146882da
Taifuuni <h3tulus@hotmail.com>
2024-04-07 20:04:03 +0300

    Add and refactor Overworld chunks for Grasslands and Rocky Hills areas

---
+++ Overworld/Overworld Chunks/#1#22.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var locations = {
+	"Donnafolk Caves": {
+		"tiles": PackedVector2Array([
+			Vector2i(7, 28)
+		]),
+		"pregenerated": true
+	}
+}
--- Overworld/Overworld Chunks/#8#17.gd
+++ Overworld/Overworld Chunks/#8#17.gd
@@ -3,10 +3,7 @@ extends Node2D
 var locations = {
 	"Lazysprings": {
 		"tiles": PackedVector2Array([
-			Vector2i(20, 35),
-			Vector2i(20, 36),
-			Vector2i(21, 36),
-			Vector2i(20, 37)
+			Vector2i(20, 36)
 		]),
 		"pregenerated": true
 	}
