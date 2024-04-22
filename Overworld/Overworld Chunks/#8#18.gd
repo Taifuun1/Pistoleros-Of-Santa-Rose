80d85c321ca194f794b432f29ada9f3f4b3564aa
Taifuuni <h3tulus@hotmail.com>
2024-04-23 00:03:34 +0300

    Add and modify Grasslands and Rocky Hills chunks

--- Overworld/Overworld Chunks/#8#18.gd
+++ Overworld/Overworld Chunks/#8#18.gd
@@ -3,10 +3,7 @@ extends Node2D
 var locations = {
 	"Clearwater Grove": {
 		"tiles": PackedVector2Array([
-			Vector2i(25, 59),
-			Vector2i(25, 60),
-			Vector2i(26, 60),
-			Vector2i(25, 61),
+			Vector2i(25, 61)
 		]),
 		"pregenerated": false
 	}
