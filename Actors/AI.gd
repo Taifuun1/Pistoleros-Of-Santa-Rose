a8d0eb516b049bc2341850fca2be58efa9426977
Taifuuni <h3tulus@hotmail.com>
2024-05-13 20:26:14 +0300

    Rename and delete files

--- Actors/AI.gd
+++ Actors/AI.gd
@@ -14,13 +14,13 @@ func init(newAIType):
 func _physics_process(delta):
 	if currentMovementTarget != null:
 		var direction = Vector2.ZERO
-		var localActorPosition = get_tree().current_scene.get_node("Entities/Actors/NpcActor").global_position
+		var localActorPosition = get_tree().current_scene.get_node("Entities/Actors/LocationActor").global_position
 		#localActorPosition.y += 6
 		
 		direction = currentMovementTarget - localActorPosition
 		direction = direction.normalized()
 		
-		get_tree().current_scene.get_node("Entities/Actors/NpcActor").global_position = localActorPosition.move_toward(currentMovementTarget, delta * speed)
+		get_tree().current_scene.get_node("Entities/Actors/LocationActor").global_position = localActorPosition.move_toward(currentMovementTarget, delta * speed)
 		
 		#get_tree().current_scene.get_node("Entities/Actors/NpcActor").velocity = direction * speed
 		#
@@ -29,7 +29,7 @@ func _physics_process(delta):
 		#print(currentMovementTarget)
 		#print(movementPath)
 		#print(direction)
-		if get_tree().current_scene.get_node("Entities/Actors/NpcActor").position.distance_to(currentMovementTarget) < 1:
+		if get_tree().current_scene.get_node("Entities/Actors/LocationActor").position.distance_to(currentMovementTarget) < 1:
 			currentMovementTarget = null
 		
 		return
@@ -52,8 +52,8 @@ func _on_timer_timeout():
 		match aIType:
 			"passive":
 				if randi() % 2 == 0:
-					var randomNearbyPosition = Vector2i(get_tree().current_scene.get_node("Entities/Actors/NpcActor").position) + Vector2i(((randi() % 4 + 1) + -2) * HelperVariables.tileSize.x, ((randi() % 4 + 1) + -2) * HelperVariables.tileSize.y)
+					var randomNearbyPosition = Vector2i(get_tree().current_scene.get_node("Entities/Actors/LocationActor").position) + Vector2i(((randi() % 4 + 1) + -2) * HelperVariables.tileSize.x, ((randi() % 4 + 1) + -2) * HelperVariables.tileSize.y)
 					if get_tree().current_scene.has_node("Map"):
-						movementPath = get_tree().current_scene.get_node("AI/Pathfinding").calculatePath(pathfindingAstarNode, get_tree().current_scene.get_node("Map").local_to_map(to_local(get_tree().current_scene.get_node("Entities/Actors/NpcActor").position - Vector2(0, HelperVariables.tileSize.y / 2))), get_tree().current_scene.get_node("Map").local_to_map(to_local(randomNearbyPosition)))
+						movementPath = get_tree().current_scene.get_node("AI/Pathfinding").calculatePath(pathfindingAstarNode, get_tree().current_scene.get_node("Map").local_to_map(to_local(get_tree().current_scene.get_node("Entities/Actors/LocationActor").position - Vector2(0, HelperVariables.tileSize.y / 2))), get_tree().current_scene.get_node("Map").local_to_map(to_local(randomNearbyPosition)))
 					checkMovementTarget()
 				$Timer.start(3)
