a2ce3e574b42523152a6ce096d34d2f69ce009c8
Taifuuni <h3tulus@hotmail.com>
2025-01-02 19:34:35 +0200

    Fix turn order sorting function

--- Globals/HelperFunctions.gd
+++ Globals/HelperFunctions.gd
@@ -5,9 +5,9 @@ func isOutSideTileMap(point):
 	return point.x < 0 or point.y < 0 or point.x >= HelperVariables.locationChunkSize.x or point.y >= HelperVariables.locationChunkSize.y
 
 func sortToTurnOrder(a, b) -> bool:
-	if a.highTailin == b.highTailin:
+	if int(a.highTailin) == int(b.highTailin):
 		if randi() % 2 == 0:
 			return true
 		else:
 			return false
-	return a.highTailin < b.highTailin
+	return int(a.highTailin) > int(b.highTailin)
