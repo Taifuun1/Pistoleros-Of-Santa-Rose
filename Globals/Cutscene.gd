59cfcd521323360b8bb2d52aad0b147c49e3d148
Taifuuni <h3tulus@hotmail.com>
2025-09-24 23:33:37 +0300

    Add playedCutscenes variable to Cutscene global script

--- Globals/Cutscene.gd
+++ Globals/Cutscene.gd
@@ -2,3 +2,11 @@ extends Node
 
 var cutsceneArc = "Pistoleros Regroup"
 var cutsceneName = "Finch Pond"
+
+var playedCutscenes = {
+	"Finch Pond": false,
+	"Walker finds Gwyneth": false,
+	"Great Fall Ridge": false,
+	"Blackberry Retreat": false,
+	"Littehill Grove": false
+}
