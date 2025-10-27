7d209b0bcc95efe4ca241d8d755e255f1e26a614
Taifuuni <h3tulus@hotmail.com>
2025-10-27 19:27:41 +0200

    Add example data set for random Shootouts

---
+++ Data/Fight/SqueekyRat.gd
@@ -0,0 +1,72 @@
+extends Node
+
+var fight = [
+	{
+		"front": {
+			0: "Squeeky rat",
+			1: "Squeeky rat",
+			2: null
+		},
+		"middle": {
+			0: null,
+			1: null,
+			2: null
+		},
+		"back": {
+			0: null,
+			1: null,
+			2: null
+		}
+	},
+	{
+		"front": {
+			0: "Squeeky rat",
+			1: null,
+			2: null
+		},
+		"middle": {
+			0: null,
+			1: null,
+			2: null
+		},
+		"back": {
+			0: null,
+			1: null,
+			2: null
+		}
+	},
+	{
+		"front": {
+			0: null,
+			1: "Squeeky rat",
+			2: "Squeeky rat"
+		},
+		"middle": {
+			0: null,
+			1: null,
+			2: null
+		},
+		"back": {
+			0: null,
+			1: null,
+			2: null
+		}
+	},
+	{
+		"front": {
+			0: "Squeeky rat",
+			1: "Squeeky rat",
+			2: "Squeeky rat"
+		},
+		"middle": {
+			0: null,
+			1: null,
+			2: null
+		},
+		"back": {
+			0: null,
+			1: null,
+			2: null
+		}
+	}
+]
